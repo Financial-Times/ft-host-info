917a29e1cc8cade712ff5b005ff261cd26d1fe7b
Rob.Godfrey <Rob.Godfrey@ft.com>
2014-06-27 12:23:36 +0100

    Moved factory util classes for HostLocations for FT and AWS data centres from ft-async-message

---
+++ src/main/java/com/ft/platform/hostinfo/AWSHostLocations.java
@@ -0,0 +1,9 @@
+package com.ft.platform.hostinfo;
+
+public class AWSHostLocations {
+
+	// AWS
+	public static final HostLocation AWS_NORTHERN_VIRGINIA_US_EAST = new HostLocation("e1a-us");
+	public static final HostLocation AWS_IRELAND_EU = new HostLocation("w1a-eu");
+	
+}
---
+++ src/main/java/com/ft/platform/hostinfo/FTDataCentreHostLocations.java
@@ -0,0 +1,18 @@
+package com.ft.platform.hostinfo;
+
+public class FTDataCentreHostLocations {
+
+	// Park Royal UK
+	public static final HostLocation PARK_ROYAL_UK = new HostLocation("pr-uk");
+	public static final HostLocation PR_UK = PARK_ROYAL_UK;
+
+	// Imperial Way / Watford UK
+	public static final HostLocation IMPERIAL_WAY_UK = new HostLocation("iw-uk");
+	public static final HostLocation IW_UK = IMPERIAL_WAY_UK;
+	public static final HostLocation WATFORD_UK = IMPERIAL_WAY_UK;
+
+	// New Jersey US
+	public static final HostLocation NEW_JERSEY_US = new HostLocation("nj-us");
+	public static final HostLocation NJ_US = NEW_JERSEY_US;
+
+}
